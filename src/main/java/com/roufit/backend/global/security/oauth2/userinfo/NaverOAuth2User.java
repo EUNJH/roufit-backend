00ce32a11e211c7197e0fdf7a65c4f7ff03fe095
JongTTing <ejh990521@gmail.com>
2023-09-01 00:27:15 +0900

    userInfo 구현

---
+++ src/main/java/com/roufit/backend/global/security/oauth2/userinfo/NaverOAuth2User.java
@@ -0,0 +1,53 @@
+package com.roufit.backend.global.security.oauth2.userinfo;
+
+import java.util.Map;
+
+public class NaverOAuth2User extends OAuth2UserInfo {
+
+    public NaverOAuth2User(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getOAuth2Id() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        if (response == null) {
+            return null;
+        }
+        return (String) response.get("id");
+    }
+
+    @Override
+    public String getEmail() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        if (response == null) {
+            return null;
+        }
+
+        return (String) response.get("email");
+    }
+
+    @Override
+    public String getNickname() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        if (response == null) {
+            return null;
+        }
+
+        return (String) response.get("nickname");
+    }
+
+    @Override
+    public String getProfileImage() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        if (response == null) {
+            return null;
+        }
+
+        return (String) response.get("profile_image");
+    }
+}
