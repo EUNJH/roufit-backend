986476aaf986a5ad82edd62b4f144b1603e2a0bc
JongHyeok Eun <ejh990521@gmail.com>
2023-09-01 00:45:15 +0900

    Revert "workout 저장"

--- src/main/java/com/roufit/backend/domain/workout/dao/WorkoutRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.roufit.backend.domain.workout.dao;
-
-import com.roufit.backend.domain.workout.domain.record.WorkoutRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkoutRecordRepository extends JpaRepository<WorkoutRecord, Long> {
-}
