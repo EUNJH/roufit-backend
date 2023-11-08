bf3bb14bef4b2d86598c63e5f2d4725e00bd8beb
JongTTing <ejh990521@gmail.com>
2023-11-08 10:13:23 +0900

    [fix] builder 추가

--- src/main/java/com/roufit/backend/domain/workout/dto/request/WorkoutTemplateRequest.java
+++ src/main/java/com/roufit/backend/domain/workout/dto/request/WorkoutTemplateRequest.java
@@ -3,11 +3,13 @@ package com.roufit.backend.domain.workout.dto.request;
 import com.roufit.backend.domain.user.domain.User;
 import com.roufit.backend.domain.workout.domain.template.WorkoutTemplate;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Schema(description = "워크아웃 템플릿 요청(이름)")
+@Builder
 @Data
 public class WorkoutTemplateRequest {
 
